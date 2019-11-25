93620cea86a501a0dafa2030c0a9eec2c14aa572
safaa <khadim.safaa@gmail.com>
2019-11-26 00:49:52 +0100

    add the header

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,23 +1,28 @@
 import React, { Component }  from 'react';
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import { Provider } from 'react-redux';
 
 import BriefForm from './components/Briefform';
 import BriefList from './components/Brieflist';
+import Header from "./components/header/header"
 
 import store from './store';
 
 function App() {
-  return (
-    <Provider store={store}>
-        <div className="App">
 
-            <h1>Welcome to React</h1>
-            <BriefForm/>
-            <BriefList/>
-        </div>
-      </Provider>
+  return (
+  <Provider store={store} >
+    <Router>
+      <div className="App">
+        <Header />
+        <br/>
+        <Route path="/" exact component={BriefForm} />
+        <Route path="/list" exact component={BriefList} />
+      </div>
+    </Router>
+  </Provider>
   );
 }
 
